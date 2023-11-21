7bd2645d839361796c4077c0ba53f19a9254a315
Kapila Shobit <103351160+ShobitKapila@users.noreply.github.com>
2023-11-21 08:30:03 +0530

    Update collapsed_building.py

--- collapsed_building.py
+++ collapsed_building.py
@@ -4,11 +4,9 @@ import os
 from sklearn.metrics import confusion_matrix
 import numpy as np
 
-# Load the collapsed building and normal image datasets
 collapsed_building_image_files = os.listdir('collapsed_building')
 normal_image_files = os.listdir('normal')
 
-# Create a TensorFlow model with 4 dense layers
 model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
     tf.keras.layers.MaxPooling2D((2, 2)),
@@ -34,10 +32,8 @@ for image_file in collapsed_building_image_files:
 collapsed_building_image_dataset = np.array(collapsed_building_image_dataset)
 model.fit(collapsed_building_image_dataset, np.ones(len(collapsed_building_image_dataset)), epochs=10)
 
-# Save the trained model
 model.save('collapsed_building_detection_model.h5')
 
-# Load the trained model and resize the test images to the same size as the input shape of the model
 test_image_dataset = []
 for image_file in normal_image_files:
     image = cv2.imread(f'normal/{image_file}')
@@ -46,14 +42,11 @@ for image_file in normal_image_files:
 
 test_image_dataset = np.array(test_image_dataset)
 
-# Predict the labels for the test images
 predictions = model.predict(test_image_dataset)
 
-# Calculate the confusion matrix
 true_labels = [0 if image_file.startswith('normal') else 1 for image_file in normal_image_files]
 labels = [0, 1]
 
-# Calculate the confusion matrix using a dictionary
 confusion_matrix = {}
 for label in labels:
     confusion_matrix[label] = {}
@@ -63,5 +56,4 @@ for label in labels:
 for i in range(len(true_labels)):
     confusion_matrix[true_labels[i]][predictions[i][0]] += 1
 
-# Print the confusion matrix
 print(confusion_matrix)
